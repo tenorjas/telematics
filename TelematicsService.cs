dd394620c5da55f09d2d3e94c2a3ca0d3886faa0
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-20 16:55:47 -0400

    added write to file in TelematicsService class

--- TelematicsService.cs
+++ TelematicsService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace telematics
 {
@@ -6,6 +8,21 @@ namespace telematics
     {
         void Report(VehicleInfo vehicleInfo)
         {
+            // declare variables for readability
+            int VIN = vehicleInfo.VIN;
+            double odo = vehicleInfo.odometer;
+            double consumption = vehicleInfo.consumption;
+            double lastOilChange = vehicleInfo.odoLastOilChange;
+            double liters = vehicleInfo.engineLiters;
+
+            // open file for writing
+            using (var writer = new StreamWriter(File.Open($"{VIN}.json",FileMode.OpenOrCreate)))
+            {
+                // write one vehicle's telematics data to a file
+                writer.WriteLine($"{odo},{consumption},{lastOilChange},{liters}");
+            }
+
+            // variable for reading from a file
             
         }
     }

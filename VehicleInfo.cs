f81f6c18abb36090986246dbb17fa7cc40b66f51
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-20 14:56:26 -0400

    added constructs to VehicleInfo class

--- VehicleInfo.cs
+++ VehicleInfo.cs
@@ -9,5 +9,24 @@ namespace telematics
         public double consumption {get; set;}
         public double odoLastOilChange {get; set;}
         public double engineLiters {get; set;}
+
+        public override string ToString()
+        {
+            return $"{VIN},{odometer},{consumption},{odoLastOilChange},{engineLiters}";
+        }
+
+        public VehicleInfo()
+        {
+
+        }
+
+        public VehicleInfo(string[] data)
+        {
+            VIN = int.Parse(data[0]);
+            odometer = double.Parse(data[1]);
+            consumption = double.Parse(data[2]);
+            odoLastOilChange = double.Parse(data[3]);
+            engineLiters = double.Parse(data[4]);
+        }
     }
 }
